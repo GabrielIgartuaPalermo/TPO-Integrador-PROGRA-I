35bc1c8a2226e64ea45938a67ebf6933a3e26280
GabrielIgartuaPalermo <gabriel.igartua@uade.edu.ar>
2026-08-31 11:24:13 -0300

    Descripción de lo que hiciste

--- TPO PROGRA I.py
+++ TPO PROGRA I.py
@@ -1,5 +1,5 @@
 #Modulos
-from datetime import datetime
+import random
 #Sistema de Venta de entradas para Espectaculos
 #FUNCIONES
 def GuardarPrecioEntrada (Precio,Precios=[]):
@@ -11,19 +11,25 @@ def Menu ():
     print("3-Ver clientes")
     
     return
-#Profesor esta hecho con IA esta funcion
-def VerificarFormatoFecha(FechaE):
-    es_valida = False
-    while not es_valida:
-        FechaE = input("Ingresar fecha que cumpla el formato(D/M/A):")
-        try:
-            fecha_obj = datetime.strptime(FechaE, "%d/%m/%Y")
-            es_valida = True  
-            print("La fecha respeta el formato D/M/A y es válida.")
-        except ValueError:
-            print("Formato o fecha inválida. Intentá de nuevo.\n")
+def VerificarFormatoFecha(DiaD,MesM,AñoA):
+    if AñoA % 400 == 0:
+        if MesM == 2:
+            while DiaD <1 or DiaD >29:
+                print("Dia invalido, ingreselo nuevamente a continuacion")
+                DiaD = int(input("Ingresar DiaD nuevamente entre 1 - 29 incluido el 29: "))
+        if MesM <1 or MesM > 12:
+            print("Mes invalido,Ingresar nuevamente el mes, entre 1 - 12 a continuacion ")
+            MesM = int(input("Ingresar mes:"))
+    else:
+        if MesM ==2:
+            while DiaD <1 or DiaD >=29:
+                print("Dia Invalido, Ingresar nuevamente el dia a continuacion")
+                DiaD = int(input("Ingresar DiaD nuevamente entre 1 - 29 sin incluir el 29:"))
+                
+        
     
-    return FechaE
+    
+    return
 def GuardarFechaEspectaculo (FechaE,Espectaculos=[]):
     Espectaculos.append(FechaE)
     return 
@@ -37,11 +43,13 @@ seguir = "Si"
 while seguir != "No":
     Menu() 
     precioEntrada = float(input("Ingresar precio: "))
-    Fecha = input("Ingresar fecha segun formato (D/M/A (Respetar / cuando escribes la fecha) ): ")
+    Dia = int(input("Ingrese dia de 1 a 31:"))
+    Mes = int(input("Ingrese mes del 1 al 12:"))
+    Año = int(input("Ingrese año del 2026 en adelante"))
     Opcion = int(input("Ingresar una opcion"))
     GuardarPrecioEntrada(precioEntrada,ListadePreciosdeEntradas)
     GuardarFechaEspectaculo(Fecha,Fechas_Espectaculos)
-    VerificarFormatoFecha(Fecha)
+    VerificarFormatoFecha(Dia,Mes,Año)
     seguir = input("Deseas seguir? (Ingrese Si para seguir o No para no seguir:")
     if seguir == "Si":
         print("Continuamos")
